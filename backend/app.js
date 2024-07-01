cb8f06b6fd4e8f13ac7cbcb410f5f3410129c574
yazanateer <yazan.ateerr@gmail.com>
2024-07-01 13:29:38 +0300

    changes

--- backend/app.js
+++ backend/app.js
@@ -16,9 +16,6 @@ app.use(cors());
 
 app.post('/register', async (req, res) => {
   const { username, email, password, re_pass } = req.body;
-  
- 
-
   if(password != re_pass) {
     return res.status(400).json({message: 'passwords doesnt match !'});
   }
@@ -66,18 +63,95 @@ app.post('/register', async (req, res) => {
 });
 
 
+app.get('/checkUsername', async(req,res) => {
+
+  const username = req.query.username
+  console.log('debug');
+  const check_query = 'SELECT COUNT(*) AS count FROM users WHERE username = ?';
+  database.query(check_query, [username], (err, result) => {
+    if(err) {
+      console.log('error while checking the username:', err);
+      return res.status(500).send('Server error');
+    }
+
+    const users_count = result[0].count;
+    if(users_count > 0) {
+      res.status(200).send('username founded success, now change the password ');
+    } else {
+      res.status(404).send('username doesnt exist in the database');
+    }
+  });
+
+});
 
 
-//the function to check if the passowrd consists of the configurations rules 
-// function valid_passowrd(pass) {
-//   if(pass.length < config.password.min_password_length 
-//     || !(/[A-Z]/.test(pass)) ||  //upeer letters
-//     !(/[a-z]/.test(pass)) ||  //lower letters
-//     !(/\d/.test(pass)) || // have a number
-//     !(new RegExp(`[${config.password.special}]`).test(pass))
-//   ) return false;
-//   return true;
-// }
+app.post('/changePassword', async (req, res) => {
+  const { username, currentPassword, newPassword } = req.body;
+
+  try {
+    // Check if the user exists
+    const user_query = 'SELECT * FROM users WHERE username = ?';
+    database.query(user_query, [username], async (err, results) => {
+      if (err) {
+        console.error('Error in getting the user: ', err);
+        return res.status(500).send('Server error');
+      }
+
+      if (results.length === 0) {
+        return res.status(404).json({ message: 'Username does not exist' });
+      }
+
+
+      const user = results[0];
+
+      //check if the current password match the saved in the database 
+      //const is_match_pass = await bcrypt.compare(currentPassword, user.password);
+      console.log(currentPassword);
+      console.log(user.password);
+      // if (!is_match_pass) {
+      //   return res.status(400).json({ message: 'Current password is incorrect' });
+      // }
+
+      //check if the new password match the configurations 
+      if (!valid_password(newPassword)) {
+        return res.status(400).json({ message: 'The new password is too weak' });
+      }
+
+
+      //update the new password in the database using salt and hmac
+      const salt = await bcrypt.genSalt(10);
+      const hadshed_password = await bcrypt.hash(newPassword, salt);
+      const update_db = 'UPDATE users SET password = ? WHERE username = ?';
+      database.query(update_db, [hadshed_password, username], (err,result) => {
+        if(err){
+          console.log('Error updating the password ', err);
+          return res.status(500).send('server error');
+
+      }
+    res.status(200).json({message: 'password changed succdssfully in the database'});
+    
+     });
+
+
+  });
+  
+  
+}catch{
+  console.error('Error changing password: ', error);
+  res.status(500).send('Server error');
+}
+});
+
+
+
+
+
+
+
+
+
+
+
 
 
 function valid_password(pass) {
@@ -100,14 +174,9 @@ function valid_password(pass) {
     console.log('Password lacks number');
     return false;
   }
-  // if (!new RegExp(`[${config.password.special}]`).test(pass)) {
-  //   console.log('Password lacks special character');
-  //   return false;
-  // }
-  
+ 
   const special_chars = [...config.password.special];
-  const regexPattern = new RegExp(`[${special_chars}]`);
-  if(!regexPattern.test(pass)){
+  if(!(hasSpecialCharacter(pass, special_chars))){
     console.log('Password lacks special characters');
     return false;
   }
@@ -116,6 +185,15 @@ function valid_password(pass) {
   return true;
 }
 
+
+function hasSpecialCharacter(password, specialCharacters) {
+  for (let char of password) {
+      if (specialCharacters.includes(char)) {
+          return true;
+      }
+  }
+  return false;
+}
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);

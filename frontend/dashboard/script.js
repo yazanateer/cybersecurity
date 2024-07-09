89305648f15339dc515962fb74c8a7f8b4253618
yazanateer <yazan.ateerr@gmail.com>
2024-07-09 21:47:06 +0300

    added the navbar...

--- frontend/dashboard/script.js
+++ frontend/dashboard/script.js
@@ -43,7 +43,7 @@ document.addEventListener('DOMContentLoaded', () => {
         console.error('Error fetching customers:', error.message);
       }
     };
-  
+
     newCustomerForm.addEventListener('submit', async (event) => {
       event.preventDefault();
   
